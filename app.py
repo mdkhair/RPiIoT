819b97010a32e9a5aa9f0425e09e90b19f99540e
Ibnu Noordin <129460438+mdkhair@users.noreply.github.com>
2025-07-31 16:14:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,42 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+import Adafruit_DHT
 import eventlet
-import random
 import time
 
+# Enable cooperative threading
 eventlet.monkey_patch()
 
+# Flask app and SocketIO setup
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+# DHT11 sensor setup
+DHT_SENSOR = Adafruit_DHT.DHT11
+DHT_PIN = 4  # Use your GPIO pin here
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html') 
 
-# Background task to emit data every 2 seconds
-def generate_sensor_data():
+# Background task to read from DHT11 and emit data
+def read_dht_sensor():
     while True:
-        temp = round(random.uniform(25, 35), 2)
-        humidity = round(random.uniform(40, 60), 2)
-        socketio.emit('sensor_data', {'temp': temp, 'humidity': humidity})
+        humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+        if humidity is not None and temperature is not None:
+            socketio.emit('sensor_data', {
+                'temp': round(temperature, 1),
+                'humidity': round(humidity, 1)
+            })
+        else:
+            print("Failed to retrieve data from DHT11 sensor")
         time.sleep(2)
 
-# Start background thread after server starts
+# Start background thread when client connects
 @socketio.on('connect')
-def connected():
+def on_connect():
     print("Client connected.")
-    socketio.start_background_task(generate_sensor_data)
+    socketio.start_background_task(read_dht_sensor)
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000)
- 
